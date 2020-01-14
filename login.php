6ff30564882ef76fc41c2bd4dd21aebf14402256
Debfront <deborah_monteiro12@hotmail.com>
2020-01-14 20:19:24 -0300

    14/jan
    Tela de login e pegar ip do usuario

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sistema Mercado </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  
+</head>
+<body>
+
+<form>
+  <div class="form-group">
+    <label> SistMercado!!!!!!</label> </br>
+    <label> Login: </label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder=" Digite seu email">
+    <small id="emailHelp" class="form-text text-muted"></small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Senha</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder=" Digite sua senha">
+  </div>
+  </div>
+  <button type="submit" class="btn bg-warning">Acesse</button>
+</form>
+</body>
+</html>
+
+
+
+
+
---
+++ pegarip.php
@@ -0,0 +1,25 @@
+<?php
+$ipaddress = $_SERVER["SERVER_ADDR"];
+
+function infus($ip) {
+    $json = file_get_contents('http://ipinfo.io/');
+    $info = json_decode($json);
+    return $info;
+}
+
+$details = infus($ipaddress);
+//echo "Seu ip:   ".$details->ip."<br>"; 
+//echo "Sua cidade: ".$details->city."<br>";
+//echo "Sua região é: ".$details->region."<br>";
+$ip= $details->ip;
+//echo $ip;
+
+if($ip == "186.225.28.128"){
+    echo" Redirecionando para o site";
+    header('Location: index.php');
+}
+if($ip =="25.25.25"){
+    echo" Redirecionando";
+    header('Location: teste.php');
+}
+?>
