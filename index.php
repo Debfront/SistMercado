d59ce7b2c99278ec272140fc3265801394aa9ce3
Debfront <deborah_monteiro12@hotmail.com>
2020-01-15 20:11:37 -0300

    tela de login

--- index.php
+++ index.php
@@ -13,26 +13,28 @@
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title> Tela de Login </title>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	</head>
-	<body>
+    </head>
+    
+    
+    <body>
     <div id="login">
-        <h3 class="text-center text-white pt-5"> Login</h3>
+        <h3 class="text-center text-white pt-5"> SistMercado</h3>
         <div class="container">
             <div id="login-row" class="row justify-content-center align-items-center">
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
                         <form id="login-form" class="form" action="" method="post">
-                            <h3 class="text-center text-info"> SistMercado</h3>
+                            <h3 class="text-center text-info">SistMercado</h3>
                             <div class="form-group">
-                                <label for="username" class="text-info"> Usuário:</label><br>
+                                <label for="username" class="text-info">Usuário:</label><br>
                                 <input type="text" name="username" id="username" class="form-control">
                             </div>
                             <div class="form-group">
-                                <label for="password" class="text-info"> Senha:</label><br>
+                                <label for="password" class="text-info">Senha:</label><br>
                                 <input type="text" name="password" id="password" class="form-control">
                             </div>
                             <div class="form-group">
-                                <label for="remember-me" class="text-info"><span> Lembre-me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
+                                <label for="remember-me" class="text-info"><span>Lembre-me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
                                 <input type="submit" name="submit" class="btn btn-info btn-md" value="Acesse">
                             </div>
                             <div id="register-link" class="text-right">
@@ -45,6 +47,7 @@
         </div>
     </div>
 </body>
+
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
